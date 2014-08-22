512eab14de53c252afa111b08c73734263c111e9
Corey White <smortopahri@gmail.com>
2014-08-22 16:06:06 -0400

    Added encoding

--- app/scripts/controllers/find.js
+++ app/scripts/controllers/find.js
@@ -5,10 +5,11 @@ angular.module('parkingApp')
     function($scope, $http){
     	$scope.getDirections = function (data) {
     		$scope.directions = null;
-    		var conf = {origin: data.from, destination: data.to};
+    		var conf = {origin: data.from, destination: data.to region: us};
     		$http.get('https://maps.googleapis.com/maps/api/directions/json?callback=JSON_CALLBACK', {params: conf}).success(function(res){
     			console.log(res);
     			$scope.directions = data;
+                // var polyline = L.Polyline.fromEncoded(encoded);
     		});
     	}
     	
